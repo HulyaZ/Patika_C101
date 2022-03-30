64625a7a468e0060ba5f3db4083a589338b85ab2
Hulya <zeylan.hulya@gmail.com>
2022-03-30 12:57:20 +0300

    init
    Patika C# 101

---
+++ Patika_C101_Degiskenler/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            byte byte01 = 5;        // 01 Byte
+            sbyte sbyte01 = 5;      // 01 Byte
+
+            short short01 = 5;      // 01 Byte
+            ushort ushort01 = 5;    // 01 Byte
+
+            //Integers
+
+            Int16 nt16 = 2;         // 02 Byte
+            Int32 nt32 = 2;         // 04 Byte
+            Int64 nt64 = 2;         // 08 Byte  
+            int i = 2;              // 04 Byte
+
+            uint uint01 = 2;         // 04 Byte
+            long long01 = 4;         // 08 Byte
+            ulong ulong01 = 4;       // 08 Byte
+
+
+            //Real Nums
+            float float01 = 5;        // 04 Byte
+            double double01 = 5;      // 08 Byte
+            decimal decimal01 = 5;    // 016 Byte
+
+
+            //
+
+            char char01 = 'H';
+            string string01 = "Hello,";
+
+            string name = "James";
+            string surname = "Black";
+            string fullName = name + " " + surname;
+
+            object obj01 = null;
+
+            bool bool01 = true;
+
+            //Conversions
+
+            string str02 = "20";
+            int int02 = 20;
+
+            string newValue = str02 + " " + int02.ToString();
+            Console.WriteLine(newValue);
+
+            int int03 = int02 + int.Parse(str02);
+
+            //Time & Date Time
+
+            DateTime dt = DateTime.Now;
+            string datetime = DateTime.Now.ToString("dd,MM,yy");
+            Console.WriteLine(datetime);
+
+        }
+    }
+}
+
---
+++ Patika_C101_Operatorler/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Operators
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 3;
+            int y = 3;
+
+            y = y + 2;
+            Console.WriteLine(y);
+
+            y += 2;
+            Console.WriteLine(y);
+
+            y /= 1;
+            Console.WriteLine(y);
+
+            x++;
+            Console.WriteLine(x + y);
+
+
+            //Logic Operators
+            // ||  && !
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted) // works for true - true
+            {
+                Console.WriteLine("Perfect!");
+            }
+
+            if (isSuccess || isCompleted) // works if either one is true
+            {
+                Console.WriteLine("Great!");
+            }
+
+            if (isSuccess && !isCompleted) // works for true - false
+            {
+                Console.WriteLine("Fine");
+            }
+
+            //Relational Operators
+            // < > <= >= == !=
+
+            int a = 3;
+            int b = 4;
+            bool result = a < b;
+            Console.WriteLine("result for a < b " + result);
+            result = a > b;
+            Console.WriteLine("result for a > b " + result);
+            result = a >= b;
+            Console.WriteLine("result for a >= b " + result);
+            result = a <= b;
+            Console.WriteLine("result for a <= b " + result);
+            result = a == b;
+            Console.WriteLine("result for a == b " + result);
+            result = a != b;
+            Console.WriteLine("result for a != b " + result);
+
+
+            //Arithmetic operators
+            // + - * /
+
+            int num01 = 10;
+            int num02 = 5;
+
+            int result01 = num01 + num02;
+            Console.WriteLine(num01 + " + " + num02 + " = " + result01);
+
+            result01 = num01 - num02;
+            Console.WriteLine(num01 + " + " + num02 + " = " + result01);
+
+            result01 = num01 / num02;
+            Console.WriteLine(num01 + " + " + num02 + " = " + result01);
+
+            result01 = num01 * num02;
+            Console.WriteLine(num01 + " + " + num02 + " = " + result01);
+
+            num01++;
+            Console.WriteLine(num01);
+
+            // Mod %
+            int res02 = 20 % 3;
+            Console.WriteLine(" 20(mod3) = " + res02);
+        }
+    }
+}
