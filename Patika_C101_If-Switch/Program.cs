55315f97a461c7f8d8561f0c3ef88766097d3bb3
Hulya <zeylan.hulya@gmail.com>
2022-03-30 15:01:15 +0300

    3rd commit
    _Debugging
    _If / Switch

---
+++ Patika_C101_If-Switch/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // If
+
+            int time = DateTime.Now.Hour;
+
+            if (time > 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın!");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("Iyi günler!");
+            }
+            else
+            {
+                Console.WriteLine("Iyi geceler!");
+            }
+
+            string result = time < 18 ? "Second Statement" : "Last Statement";
+            result = time >= 6 && time < 11 ? "First Statement" : time < 18 ? "Second Statement" : "Last Statement";
+            Console.WriteLine(result);
+
+            // Switch
+
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                default:
+                    Console.WriteLine("Non-existing");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Winter season");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring season");
+                    break;
+                default :
+                    break;  
+
+            }
+        }
+    }
+}
---
+++ Patika_C101_TryCatch_Debug/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             /*
+            try
+            { 
+                Console.WriteLine("Please insert a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("number = " + num);
+            }            
+           
+            catch(Exception ex) //when error occurs
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+            }
+            
+           
+            finally // regardless of condition
+            {
+                Console.WriteLine("Completed.");
+
+            }
+            */
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-123456789123456789");
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("null value: ");
+                Console.WriteLine(ex);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("invalid type: ");
+                Console.WriteLine(ex);
+            }
+
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("out of bounds: ");
+                Console.WriteLine(ex);
+            }
+
+        }
+        }
+
+}
