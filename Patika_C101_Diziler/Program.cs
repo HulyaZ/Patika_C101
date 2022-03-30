217a528cedf7938b4b1fd8abb0bac5bc242c5cee
Hulya <zeylan.hulya@gmail.com>
2022-03-30 16:37:43 +0300

    4th commit
    _For / Foreach / While
    _Arrays

---
+++ Patika_C101_Diziler/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            /*
+            string[] colors = new string[5];
+            colors[0] = "Blue";
+
+
+            int[] arr;
+            arr = new int[5];
+            arr[3] = 10;
+            
+            Console.WriteLine(colors[0]);
+            Console.WriteLine(arr[3]);
+            
+
+            Console.WriteLine("Dizinin eleman sayısı?");
+            int arrLength = int.Parse(Console.ReadLine());
+            int[] arrNum = new int[arrLength];
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write("Lütfen {0} sayıyı giriniz: ", i+1);
+                arrNum[i] = int.Parse(Console.ReadLine());  
+            }
+
+            int sum = 0;
+            foreach (int i in arrNum)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("Dizinin elemanlarının ortalaması: " + (sum/arrLength));
+
+            */
+
+            int[] arrNums = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+           
+            //Sort
+            Array.Sort(arrNums);
+            foreach (int num in arrNums)
+            {
+                Console.WriteLine(num);
+            }
+                     
+ 
+            //Reverse
+            Array.Reverse(arrNums);
+            foreach (int num in arrNums)
+            {
+                Console.WriteLine(num);
+            }
+         
+            //Index Of
+            Console.WriteLine(Array.IndexOf(arrNums, 23));
+
+            //Resize
+            Array.Resize<int>(ref arrNums, 9);
+            arrNums[8] = 99;
+            foreach (int num in arrNums)
+            {
+                Console.WriteLine(num);
+            }
+
+            //Clear
+            Array.Clear(arrNums, 2, 1); // resets one element starting from the nth to 0  
+            foreach (int num in arrNums)
+            {
+                Console.WriteLine(num);
+            }
+        
+        }
+
+    }
+         
+
+}
---
+++ Patika_C101_ForLoop-While/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class Program
+{
+    static void Main (String[] args)
+    {
+        #region //For Loop
+
+        /*
+        Console.WriteLine("Please insert a number; ");
+        int count = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i < count; i++)
+        {
+            if(i%2 == 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        int oddTotal = 0;
+        int evenTotal = 0;
+
+        for (int i = 1; i < 1000; i++)
+        {
+            if (i % 2 == 0)   
+                evenTotal += i;
+            else
+                oddTotal += i;
+        }
+        Console.WriteLine("even total = " + evenTotal);
+        Console.WriteLine("odd total = " + oddTotal);
+       
+
+        for (int i = 1; i < 10; i++)
+        {
+            if( i == 6)
+            {
+                break;
+            }
+            Console.WriteLine(i);
+        }
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (i == 6)
+            {
+                continue;
+            }
+            Console.WriteLine(i);
+        }
+        */
+        #endregion
+
+        #region//While - Foreach
+
+        int num = int.Parse(Console.ReadLine());
+        int count = 1;
+        int sum = 0;
+        while ( count < num)
+        {
+            sum += count;
+            count++;
+        }
+        Console.WriteLine(sum/num);
+
+
+        char char0 = 'a';
+
+        while (char0 < 'z')
+        {
+            Console.Write(char0);
+            char0++;
+        }
+
+
+        string[] cars = { "BMW", "Ford", "Kia" };
+        {
+            foreach (string c in cars)
+            {
+                Console.Write(c + " ");
+            }
+
+
+        }
+        #endregion
+
+    }
+
+
+}
